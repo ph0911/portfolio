eb0c377c693d46763b11c2486bbc912ad06fecce
ph0911 <102967961+ph0911@users.noreply.github.com>
2025-03-07 13:00:31 +0100

    Vercel Speed Insights fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
           cormorant.variable
         )}
       >
+        <SpeedInsights />
         <Providers>
           <Header />
           <MobileNavigationWrapper>
